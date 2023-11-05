4445e4264d632bb197d2b1c75fd26e1eace23942
stheno2010 <denkende.kissa@gmail.com>
2023-11-06 00:01:34 +0900

    modify Cats Effect code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,26 @@
 import cats.effect.{IO, IOApp}
+import scala.concurrent.duration._
 import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
 
-object HelloWorld extends IOApp.Simple {
-  val foo: Foo = Qux(13, Some(14.0))
-  val json = foo.asJson.noSpaces
-  val run = IO.println(json)
+object StupidFizzBuzz extends IOApp.Simple {
+  val run =
+    for {
+      ctr <- IO.ref(0)
+
+      wait = IO.sleep(1.second)
+      poll = wait *> ctr.get
+
+      _ <- poll.flatMap(IO.println(_)).foreverM.start
+      _ <- poll.map(_ % 3 == 0).ifM(IO.println(Qux(3, "fizz").asJson.noSpaces), IO.unit).foreverM.start
+      _ <- poll.map(_ % 5 == 0).ifM(IO.println(Qux(5, "buzz").asJson.noSpaces), IO.unit).foreverM.start
+
+      _ <- (wait *> ctr.update(_ + 1)).foreverM.void
+    } yield ()
 }
 
 sealed trait Foo
-case class Bar(xs: Vector[String]) extends Foo
 
 case class Qux(
   i: Int,
-  d: Option[Double]
+  d: String
 ) extends Foo
